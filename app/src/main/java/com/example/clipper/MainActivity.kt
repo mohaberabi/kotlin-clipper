1a3720d671fee3a4083e9a31612cc82180fe74ff
ErabiFit Inc <mohaberabi98@icloud.com>
2024-03-26 21:12:28 +0200

    first commit

---
+++ app/src/main/java/com/example/clipper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clipper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(ClippedView(context = this))
+    }
+}
